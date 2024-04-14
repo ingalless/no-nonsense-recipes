7aabf7ae5c6619bd7ffce2cfe6050a699cd9aa1a
Jonny Ingall <jonathanaingall@gmail.com>
2024-04-14 18:44:17 +0100

    Begin a basic website

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, path::PathBuf};
+
+use maud::{html, Markup};
+use rocket::http::Status;
+
+#[macro_use]
+extern crate rocket;
+
+mod views;
+
+pub struct Recipe {
+    _path: PathBuf,
+    title: String,
+}
+
+#[get("/recipe/<recipe>")]
+fn read_recipe(recipe: &str) -> Result<Markup, Status> {
+    Ok(html! {
+        p { (recipe) }
+    })
+}
+
+#[get("/")]
+fn index() -> Result<Markup, Status> {
+    let dir = fs::read_dir("./recipes");
+    if dir.is_err() {
+        println!("Could not read dir");
+        return Err(Status::InternalServerError);
+    }
+    let entries = dir
+        .unwrap()
+        .map(|res| {
+            res.map(|e| Recipe {
+                _path: e.path(),
+                title: e.file_name().to_str().unwrap().to_string(),
+            })
+        })
+        .collect::<Result<Vec<_>, std::io::Error>>();
+    return match entries {
+        Ok(recipes) => Ok(views::index(recipes)),
+        _ => Err(Status::InternalServerError),
+    };
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, read_recipe])
 }
---
+++ src/views.rs
@@ -0,0 +1,15 @@
+use maud::{html, Markup};
+
+use crate::Recipe;
+
+pub fn index(recipes: Vec<Recipe>) -> Markup {
+    html! {
+        h1 { "No Nonsense Recipes" }
+        p { "I got fed up with recipe sites with lots of ads, so I decided to build my own." }
+        ul {
+            @for recipe in recipes {
+                li { a href=(format!("/recipe/{}", recipe.title)) { (recipe.title) } }
+            }
+        }
+    }
+}
