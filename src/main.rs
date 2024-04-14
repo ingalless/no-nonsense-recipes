f2438f69f852a8fdf5a2bb25f0bcac3b6217ec69
Jonny Ingall <jonathanaingall@gmail.com>
2024-04-14 21:05:50 +0100

    Add ability to read recipes on the website
    In the future, the plan is to build the page statically as part of
    startup. Perhaps this could even become a route which can be hit by a
    webhook, which subsequently triggers a git pull for the latest content.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{fs, path::PathBuf};
 
+use comrak::Options;
 use maud::{html, Markup};
 use rocket::http::Status;
 
@@ -11,32 +12,39 @@ mod views;
 pub struct Recipe {
     _path: PathBuf,
     title: String,
+    content: String,
 }
 
 #[get("/recipe/<recipe>")]
 fn read_recipe(recipe: &str) -> Result<Markup, Status> {
+    let content = fs::read_to_string(format!("./recipes/{}", recipe)).unwrap_or(String::from(""));
     Ok(html! {
         p { (recipe) }
+        (maud::PreEscaped(comrak::markdown_to_html(&content, &Options::default())))
     })
 }
 
-#[get("/")]
-fn index() -> Result<Markup, Status> {
+fn get_recipes() -> Result<Vec<Recipe>, String> {
     let dir = fs::read_dir("./recipes");
-    if dir.is_err() {
-        println!("Could not read dir");
-        return Err(Status::InternalServerError);
-    }
     let entries = dir
         .unwrap()
         .map(|res| {
             res.map(|e| Recipe {
                 _path: e.path(),
                 title: e.file_name().to_str().unwrap().to_string(),
+                content: fs::read_to_string(e.path()).unwrap_or(String::from("")),
             })
         })
         .collect::<Result<Vec<_>, std::io::Error>>();
     return match entries {
+        Ok(recipes) => Ok(recipes),
+        Err(e) => Err(e.to_string()),
+    };
+}
+
+#[get("/")]
+fn index() -> Result<Markup, Status> {
+    return match get_recipes() {
         Ok(recipes) => Ok(views::index(recipes)),
         _ => Err(Status::InternalServerError),
     };
@@ -44,5 +52,18 @@ fn index() -> Result<Markup, Status> {
 
 #[launch]
 fn rocket() -> _ {
+    match get_recipes() {
+        Ok(recipes) => {
+            for recipe in recipes {
+                println!(
+                    "{:?}",
+                    comrak::markdown_to_html(&recipe.content, &Options::default())
+                )
+            }
+        }
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
     rocket::build().mount("/", routes![index, read_recipe])
 }
