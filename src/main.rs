481f3893fbc9ec1ce6f275e0bd3391f47df31ef4
Jonny Ingall <jonathanaingall@gmail.com>
2024-04-16 20:57:15 +0100

    Added a test
    The problem now is that the test leads .html files in the compiled
    folder. I need to abstract the compiler parts into its own module or
    trait, which configures the "compilation" directory. This would allow
    compilation going to directories _outside_ of the app, which could be
    useful if I decide to make it possible to recompile via a webhook.

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,8 @@ fn read_recipe(recipe: &str) -> Result<(Status, (ContentType, String)), Status> 
     Ok((Status::Ok, (ContentType::HTML, content)))
 }
 
-fn get_recipes() -> Result<Vec<Recipe>, String> {
-    let dir = fs::read_dir("./recipes");
+fn get_recipes(path: String) -> Result<Vec<Recipe>, String> {
+    let dir = fs::read_dir(path);
     let entries = dir
         .unwrap()
         .map(|res| {
@@ -58,7 +58,8 @@ fn get_recipes() -> Result<Vec<Recipe>, String> {
 
 #[get("/")]
 fn index() -> Result<Markup, Status> {
-    return match get_recipes() {
+    let path = std::env::var("APP_RECIPES_PATH").unwrap_or("./recipes".into());
+    return match get_recipes(path) {
         Ok(recipes) => Ok(views::index(recipes)),
         _ => Err(Status::InternalServerError),
     };
@@ -66,7 +67,8 @@ fn index() -> Result<Markup, Status> {
 
 #[launch]
 fn rocket() -> _ {
-    match get_recipes() {
+    let path = std::env::var("APP_RECIPES_PATH").unwrap_or("./recipes".into());
+    match get_recipes(path) {
         Ok(recipes) => {
             for recipe in recipes {
                 let write_result = std::fs::write(
@@ -84,4 +86,35 @@ fn rocket() -> _ {
         }
     }
     rocket::build().mount("/", routes![index, read_recipe])
+}
+
+#[cfg(test)]
+mod test {
+    use std::{fs, io};
+
+    use super::rocket;
+    use crate::rocket_uri_macro_index;
+    use rocket::form::validate::Contains;
+    use rocket::http::Status;
+    use rocket::local::blocking::Client;
+    use rocket::uri;
+    use tempdir::TempDir;
+
+    #[test]
+    fn index() -> Result<(), io::Error> {
+        let tmp_dir = TempDir::new("recipes")?;
+        let file_path = tmp_dir.path().join("soy-salmon.md");
+        fs::write(file_path, "#Honey Soy Salmon")?;
+        std::env::set_var("APP_RECIPES_PATH", tmp_dir.path());
+
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+
+        let response = client.get(uri!(index)).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        let content = response.into_string();
+        assert_eq!(content.contains("No Nonsense Recipes"), true);
+        assert_eq!(content.contains("soy-salmon"), true);
+
+        Ok(())
+    }
 }
