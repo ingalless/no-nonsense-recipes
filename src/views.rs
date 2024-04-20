c601e91b286101270a361495de62f8ad5ad78209
Jonathan Ingall <jonathanaingall@gmail.com>
2024-04-20 21:41:02 +0100

    Fix meta tag

--- src/views.rs
+++ src/views.rs
@@ -19,6 +19,7 @@ fn layout(rest: Markup) -> Markup {
 fn head() -> Markup {
     html! {
         head {
+            meta name="viewport" content="width=device-width, initial-scale=1" {}
             script src="https://cdn.tailwindcss.com?plugins=typography" {}
         }
     }
